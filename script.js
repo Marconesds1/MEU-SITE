3e5b49a31cd2859f811af1027664249ae9f74b55
Marcones <141690889+Marconesds1@users.noreply.github.com>
2025-03-09 23:09:07 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("btnWhatsApp").addEventListener("click", function() {
-    window.location.href = "https://wa.me/5598991711256";
-});
-
-document.getElementById("btnTelegram").addEventListener("click", function() {
-    window.location.href = "https://t.me/Marcones_ms";
-});
